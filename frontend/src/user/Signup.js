8067a7e394aa787d6463e9596d9925449dab8d80
cnangia <cnangia@ncsu.edu>
2022-10-02 18:28:38 -0400

    added logic for auth helper methods and signup on submit

--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -1,7 +1,44 @@
-import React from "react"
+import React, {useState} from "react"
+import { signup } from "../auth/helper";
 import Base from "../core/Base"
 
 const Signup = () => {
+
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false
+    });
+    
+    const {name, email, password, error, success} = values;
+
+    const handleChange = name => event => {
+        setValues({...values, error: false, [name]: event.target.value})
+    }
+
+    const onSubmit = event => {
+        event.preventDefault();
+        setValues({...values, error: false});
+        signup({name, email, password})
+        .then(data => {
+            if(data.error) {
+                setValues({...values, error: data.error, success: false});
+            }else{
+                setValues({
+                    ...values,
+                    name: "",
+                    email: "",
+                    password: "",
+                    error: "",
+                    success: true
+                });
+            }
+        })
+        .catch(console.log("Error in signup"));
+    }
+
     const signUpForm = () => {
         return (
             <div className="row">
@@ -9,15 +46,15 @@ const Signup = () => {
                     <form> 
                         <div className="form-group">
                             <label className="text-light">Name</label>
-                            <input className="form-control" type="text"/>
+                            <input className="form-control" onChange={handleChange("name")} type="text"/>
                         </div>
                         <div className="form-group">
                             <label className="text-light">Email</label>
-                            <input className="form-control" type="email"/>
+                            <input className="form-control" onChange={handleChange("email")} type="email"/>
                         </div>
                         <div className="form-group">
                             <label className="text-light">Password</label>
-                            <input className="form-control" type="password"/>
+                            <input className="form-control" onChange={handleChange("password")} type="password"/>
                         </div>
                         <div className="form-group">
                             <label className="text-dark">submit</label>
