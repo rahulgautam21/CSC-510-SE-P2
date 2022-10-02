15513de4f09d64ba6e0e29d98903a5010bfe9a7a
cnangia <cnangia@ncsu.edu>
2022-10-02 17:14:32 -0400

    added navbar

---
+++ frontend/src/core/Menu.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+const Menu = () => (
+    <div>
+        <ul className="nav nav-tabs bg-dark">
+            <li className="nav-item">
+                <Link className="nav-link" to="/">
+                    Home
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/">
+                    Cart
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/">
+                    Dashboard
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/">
+                    A. Dashboard
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/">
+                    Signup
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/">
+                    Sign In
+                </Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/">
+                    Signout
+                </Link>
+            </li>
+        </ul>
+    </div>
+)
+
+
+export default Menu; 
