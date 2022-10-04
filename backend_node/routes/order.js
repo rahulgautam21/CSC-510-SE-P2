977519da508911ba9db14e13e08ff4ef4a9e69a2
ypatil <ypatil@ncsu.edu>
2022-10-04 17:02:43 -0400

    Creating order routes

---
+++ backend_node/routes/order.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
+const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
+const { updateStock } = require("../controllers/product");
