8478b8f5963c22e5c0d9244f2128cf0d0517511d
Saswat Priyadarshan <psaswat21@gmail.com>
2022-10-04 02:46:14 -0400

    Creating user routes

--- backend_node/app.js
+++ backend_node/app.js
@@ -7,9 +7,8 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-//My routes
 const authRoutes = require("./routes/auth");
-// const userRoutes = require("./routes/user");
+const userRoutes = require("./routes/user");
 // const categoryRoutes = require("./routes/category");
 // const productRoutes = require("./routes/product");
 // const orderRoutes = require("./routes/order");
@@ -32,9 +31,8 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
-//My Routes
 app.use("/api", authRoutes);
-// app.use("/api", userRoutes);
+app.use("/api", userRoutes);
 // app.use("/api", categoryRoutes);
 // app.use("/api", productRoutes);
 // app.use("/api", orderRoutes);
