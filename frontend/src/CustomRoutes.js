1331979d82ac188657b0ee9f191fede9f762912d
Sneha Madle <snehamadle@Snehas-Air.lan>
2022-10-05 01:39:42 -0400

    Corrected the custom route for dashboard

--- frontend/src/CustomRoutes.js
+++ frontend/src/CustomRoutes.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, ProtectedRoute } from "react-router-dom";
 import Home from "./core/Home";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
-import PrivateRoute from "./auth/helper/PrivateRoutes";
 import UserDashBoard from "./user/UserDashBoard";
 
 const CustomRoutes = () => {
@@ -13,7 +12,7 @@ const CustomRoutes = () => {
         <Route path="/" exact element={<Home />}></Route>
         <Route path="/signup" exact element={<Signup />}></Route>
         <Route path="/signin" exact element={<Signin />}></Route>
-        <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
+        <Route path="/user/dashboard"  element={<UserDashBoard/ >} />
       </Routes>
     </BrowserRouter>
   );
