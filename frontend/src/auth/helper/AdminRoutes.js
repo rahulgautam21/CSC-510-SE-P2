43431935d4e0b3ca2753ad307b9a4c4d672b9954
Sneha Madle <snehamadle@Snehas-Air.lan>
2022-10-05 01:20:41 -0400

    Correct admin route errors

--- frontend/src/auth/helper/AdminRoutes.js
+++ frontend/src/auth/helper/AdminRoutes.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import { isAutheticated } from "./index";
+import { Route, Navigate } from "react-router-dom";
+import { isAuthenticated } from "./index";
 
 const AdminRoute = ({ component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
       render={props =>
-        isAutheticated() && isAutheticated().user.role === 1 ? (
+        isAuthenticated() && isAuthenticated().user.role === 1 ? (
           <Component {...props} />
         ) : (
-          <Redirect
+          <Navigate
             to={{
               pathname: "/signin",
               state: { from: props.location }
