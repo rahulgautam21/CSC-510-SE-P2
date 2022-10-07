5311cd822984b15c2a47de997446cf8704fb8176
ypatil <ypatil@ncsu.edu>
2022-10-07 17:49:51 -0400

    Disabling react/prop-types

--- backend_node/.eslintrc.js
+++ backend_node/.eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   plugins: ["react", "prettier"],
   rules: {
     "prettier/prettier": "error",
-    "no-unused-vars":"off"
+    "no-unused-vars":"off",
+    "react/prop-types": "off"
   },
 };
