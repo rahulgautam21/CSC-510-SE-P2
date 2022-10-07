2c61e7ffbec8edcf5507781e789ccd68876678e8
Sneha Madle <snehamadle@Snehas-Air.lan>
2022-10-07 01:47:43 -0400

    frontend lint changes

--- frontend/src/admin/AddCategory.js
+++ frontend/src/admin/AddCategory.js
@@ -1,86 +1,86 @@
-import React, { useState } from "react";
-import Base from "../core/Base";
-import { isAutheticated } from "../auth/helper";
-import { Link } from "react-router-dom";
-import { createCategory } from "./helper/adminapicall";
+import React, { useState } from 'react'
+import Base from '../core/Base'
+import { isAutheticated } from '../auth/helper'
+import { Link } from 'react-router-dom'
+import { createCategory } from './helper/adminapicall'
 
 const AddCategory = () => {
-  const [name, setName] = useState("");
-  const [error, setError] = useState(false);
-  const [success, setSuccess] = useState(false);
+  const [name, setName] = useState('')
+  const [error, setError] = useState(false)
+  const [success, setSuccess] = useState(false)
 
-  const { user, token } = isAutheticated();
+  const { user, token } = isAutheticated()
 
   const goBack = () => (
-    <div className="mt-5">
-      <Link className="btn btn-sm btn-success mb-3" to="/admin/dashboard">
+    <div className='mt-5'>
+      <Link className='btn btn-sm btn-success mb-3' to='/admin/dashboard'>
         Admin Home
       </Link>
     </div>
-  );
+  )
 
-  const handleChange = event => {
-    setError("");
-    setName(event.target.value);
-  };
+  const handleChange = (event) => {
+    setError('')
+    setName(event.target.value)
+  }
 
-  const onSubmit = event => {
-    event.preventDefault();
-    setError("");
-    setSuccess(false);
+  const onSubmit = (event) => {
+    event.preventDefault()
+    setError('')
+    setSuccess(false)
 
-    //backend request fired
-    createCategory(user._id, token, { name }).then(data => {
+    // backend request fired
+    createCategory(user._id, token, { name }).then((data) => {
       if (data.error) {
-        setError(true);
+        setError(true)
       } else {
-        setError("");
-        setSuccess(true);
-        setName("");
+        setError('')
+        setSuccess(true)
+        setName('')
       }
-    });
-  };
+    })
+  }
 
   const successMessage = () => {
     if (success) {
-      return <h4 className="text-success">Category created successfully</h4>;
+      return <h4 className='text-success'>Category created successfully</h4>
     }
-  };
+  }
 
   const warningMessage = () => {
     if (error) {
-      return <h4 className="text-success">Failed to create category</h4>;
+      return <h4 className='text-success'>Failed to create category</h4>
     }
-  };
+  }
 
   const myCategoryForm = () => (
     <form>
-      <div className="form-group">
-        <p className="lead">Enter the category</p>
+      <div className='form-group'>
+        <p className='lead'>Enter the category</p>
         <input
-          type="text"
-          className="form-control my-3"
+          type='text'
+          className='form-control my-3'
           onChange={handleChange}
           value={name}
           autoFocus
           required
-          placeholder="For Ex. Summer"
+          placeholder='For Ex. Summer'
         />
-        <button onClick={onSubmit} className="btn btn-outline-info">
+        <button onClick={onSubmit} className='btn btn-outline-info'>
           Create Category
         </button>
       </div>
     </form>
-  );
+  )
 
   return (
     <Base
-      title="Create a category here"
-      description="Add a new category for new tshirts"
-      className="container bg-info p-4"
+      title='Create a category here'
+      description='Add a new category for new tshirts'
+      className='container bg-info p-4'
     >
-      <div className="row bg-white rounded">
-        <div className="col-md-8 offset-md-2">
+      <div className='row bg-white rounded'>
+        <div className='col-md-8 offset-md-2'>
           {successMessage()}
           {warningMessage()}
           {myCategoryForm()}
@@ -88,7 +88,7 @@ const AddCategory = () => {
         </div>
       </div>
     </Base>
-  );
-};
+  )
+}
 
-export default AddCategory;
+export default AddCategory
--- frontend/src/core/helper/paymentbhelper.js
+++ frontend/src/core/helper/paymentbhelper.js
@@ -1,32 +1,32 @@
-import { API } from "../../backend";
+import { API } from '../../backend'
 
 export const getmeToken = (userId, token) => {
   return fetch(`${API}payment/gettoken/${userId}`, {
-    method: "GET",
+    method: 'GET',
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`
     }
   })
-    .then(response => {
-      return response.json();
+    .then((response) => {
+      return response.json()
     })
-    .catch(err => console.log(err));
-};
+    .catch((err) => console.log(err))
+}
 
 export const processPayment = (userId, token, paymentInfo) => {
   return fetch(`${API}/payment/braintree/${userId}`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`
     },
     body: JSON.stringify(paymentInfo)
   })
-    .then(reponse => {
-      return reponse.json();
+    .then((reponse) => {
+      return reponse.json()
     })
-    .catch(err => console.log(err));
-};
+    .catch((err) => console.log(err))
+}
