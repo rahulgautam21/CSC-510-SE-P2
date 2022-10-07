cb8a820d31942b250c819cf12f2c6c3ed4365ae4
Sneha Madle <snehamadle@Snehas-Air.lan>
2022-10-07 01:11:32 -0400

    Add linter for backend

---
+++ backend_node/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'env': {
+    'browser': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+  },
+};
