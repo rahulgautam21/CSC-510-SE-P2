f4fb77caebdf50b3a2998dd8902b823b336d3ff0
ypatil <ypatil@ncsu.edu>
2022-10-07 17:53:10 -0400

    Added JSDoc not required rule

--- backend_node/.eslintrc.js
+++ backend_node/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   rules: {
     "prettier/prettier": "error",
     "no-unused-vars":"off",
-    "react/prop-types": "off"
+    "react/prop-types": "off",
+    "require-jsdoc" : "off"
   },
 };
