47b7cb8b71ba45c4639a751764ced85d6563e3af
ShreyaReddy17 <43646675+ShreyaReddy17@users.noreply.github.com>
2022-12-05 15:49:55 -0500

    Update Base.js

--- frontend/src/core/Base.js
+++ frontend/src/core/Base.js
@@ -1,16 +1,17 @@
-import React from "react";
-import Menu from "./Menu";
+import React from 'react';
+import Menu from './Menu';
+import '../styles.css';
 
 const Base = ({
-  title = "Welcome to Walpaper Store  ",
-  className = "bg-blue text-white",
+  title = 'Welcome to Wallpaper Store',
+  className = 'w3-xxlarge w3-padding-large w3-opacity w3-display-container w3-content w3-wide',
   children,
 }) => (
   <div>
     <Menu />
-    <div className="container-fluid">
-      <div className="jumbotron bg-blue text-white text-center">
-        <h2 className="display-5">
+    <div >
+      <div className="w3-xxlarge w3-padding-large w3-opacity w3-display-container w3-content w3-wide">
+        <header className="display-5">
           {title}
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -23,15 +24,16 @@ const Base = ({
             <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
             <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2zM14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1zM2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1h-10z" />
           </svg>
-        </h2>
+        </header>
       </div>
       <div className={className}>{children}</div>
     </div>
     <footer className="footer bg-dark mt-auto sticky-bottom">
-      <div className="container-fluid bg-blue text-white text-left py-3">
+      <div className="container-fluid bg_blue text-white py-3">
         <h6>CSC-510: Software Engineering Project</h6>
         <h6>
-          All rights reserved{" "}
+          All rights reserved{' '}
+          
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -40,13 +42,16 @@ const Base = ({
             className="bi bi-heart-fill"
             viewBox="0 0 16 16"
           >
+            
             <path
               fillRule="evenodd"
               d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
             />
           </svg>
         </h6>
+      
       </div>
+
     </footer>
   </div>
 );
