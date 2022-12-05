6f4836222caa109c8cba011d5a56eb098524a701
ShreyaReddy17 <43646675+ShreyaReddy17@users.noreply.github.com>
2022-12-05 17:21:39 -0500

    revert changes

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -45,52 +45,6 @@ export default function Home() {
           })}
         </div>
       </div>
-      
-
-  <div className="container_card">
-    <div className="row">
-        <div className="col">
-        <div className="card">       
-                <div className="card-body">
-                  <h5 className="card-title">About Us</h5>
-                  <p className="card-text">Wanna know more about the website?
-                  Please navigate to About Us page on the navigation bar</p>
-                </div>
-              </div>
-        </div>
-        <div className="col">
-        <div className="card">     
-                <div className="card-body">
-                  <h5 className="card-title">Contact Us</h5>
-                  <p className="card-text">Do you have any query and want to talk to an expert?
-                  Please navigate to Contact Us page.</p>
-                </div>
-              </div>
-        </div>
-        <div className="col">
-        <div className="card">        
-                <div className="card-body">
-                  <h5 className="card-title">Sign up</h5>
-                  <p className="card-text">First time visting the website?
-                  Please navigate to sign up page and resgister.</p>
-                </div>
-              </div>
-        </div>
-        <div className="col">
-        <div className="card">        
-                <div className="card-body">
-                  <h5 className="card-title">Sign in</h5>
-                  <p className="card-text">Already have an account?
-                  Please navigate to sign in page and log into your account.</p>
-                </div>
-              </div>
-        </div>
-    </div>
-</div>
-<div className="row text-left">
-        <p className="bg_image"></p>
-        <h4 className="text-white text-right">Happy Shopping!!</h4>
-      </div>
     </Base>
   );
 }
